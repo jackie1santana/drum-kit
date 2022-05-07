7d86791508513b29114eb96c0372267561fbbae6
santanaj <jackie.santana@biworldwide.com>
2022-05-07 16:29:00 -0500

    finished drum kit

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     es2021: true,
     browser: true,
+    jest: true,
   },
   extends: [
     'airbnb-base',
@@ -16,11 +17,21 @@ module.exports = {
     ecmaVersion: 13,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
-    'prettier/prettier': 'error',
     'import/extensions': 'off',
     'no-console': 'off',
     'import/prefer-default-export': 'off',
+    'linebreak-style': 'off',
+    'arrow-parens': 'off',
+    'eol-last': 'off',
+    semi: 0,
+    indent: 'off',
+    'no-void': 'off',
+    'no-useless-return': 'off',
+    'no-trailing-spaces': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-debugger': 'off',
   },
 };
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,3 +1,31 @@
 import '../public/style.css';
 
-console.log('yo');
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type IEventKeys = () => void;
+
+const keyDownFunc: IEventKeys = () => {
+    window.addEventListener('keydown', (e: KeyboardEvent): void => {
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`) as HTMLAudioElement;
+        const classKey = document.querySelector(`.key[data-key="${e.keyCode}"]`) as HTMLDataElement;
+
+        if (!audio) return
+
+        audio.currentTime = 0; // rewind to the start on each keydown for the drum sounds
+        audio.play()
+
+        classKey.classList.add('playing')
+
+        //  window.addEventListener('keyup', (): void => {
+        //     classKey.classList.remove('playing')
+        // })
+        
+        const keys = document.querySelectorAll('.key') as NodeListOf<HTMLElement>
+        keys.forEach((key: HTMLElement): void => key.addEventListener('transitionend', (e: { propertyName: string; }): void => {
+            if (e.propertyName !== 'transform') return // skip if its not transform
+            
+            key.classList.remove('playing') 
+        }))
+    })
+}
+
+keyDownFunc()
